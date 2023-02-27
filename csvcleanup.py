155e19f2c1f7fccc395fee71f2303ac841a47937
Darius <67561957+Radius-coder@users.noreply.github.com>
2023-02-27 09:28:14 +0000

    Version 1.0
    Added input option so user can decide what characters to replace with commas.

--- csvcleanup.py
+++ csvcleanup.py
@@ -1,12 +1,13 @@
 #csv cleaner made to replace indents with commas by Darius Arian, 2021
 
 filename = input("Enter the file name and file tag: ")
+value = input("Enter the character you wish to replace with commas: ")
 
 file = open(filename, 'r')
 output = open('c_'+filename, 'w')
 
 for n in file:
-    output.write(','.join(n.split()))
+    output.write(value.join(n.split()))
     output.write('\n')
 file.close()
 output.close()
